0cd878d15c2f2d33563ed4ebe6eb754534740e8a
garrett-rh <garretth@tuta.io>
2021-11-09 14:46:07 +0000

    Updated hello.go to reflect the French & Spanish english requirements. Also, broke the greeting determination into its own function using a switch statement. hello_test.go also reflects these changes.

--- hello_world/v5/hello.go
+++ hello_world/v5/hello.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello returns a personalised greeting, defaulting to Hello, world if an empty name is passed.
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(langauge string) (prefix string) {
+
+	switch langauge {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	//Can just 'return' and it will return prefix since it
+	//is specified above but I don't like that
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
