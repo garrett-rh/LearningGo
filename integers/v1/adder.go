b482d3cb2f3cba28f1d7dc3fcf3030623a1a1ac9
garrett-rh <garretth@tuta.io>
2021-11-18 07:41:57 -0500

    Functioning version of adder.go

---
+++ integers/v1/adder.go
@@ -0,0 +1,5 @@
+package integers
+
+func Add(x, y int) int { //Same as Add(x int, y int) however, if both vars are of the same type, go will assume that the last type matches the first
+	return x + y
+}
