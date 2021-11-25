09782a8be79dd5d8316babd0db6d730d0bcd0a41
garrett-rh <garretth@tuta.io>
2021-11-25 14:48:46 -0500

    Removed the unused area function in favor of the area interface.

--- structs_methods_interfaces/v5/shapes.go
+++ structs_methods_interfaces/v5/shapes.go
@@ -31,6 +31,3 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
-}
