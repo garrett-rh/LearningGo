f77feaefd07d3bbddf6e7ca4dbe741e8a18471d3
Garrett <garretth@tuta.io>
2022-01-05 11:09:38 -0500

    Mocked the time.sleep() function to make the tests run faster

---
+++ mocking/v1/mock.go
@@ -0,0 +1,31 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
