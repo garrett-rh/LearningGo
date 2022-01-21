cff683cd81360708052e0abc3b170ce4ee6278b7
Garrett Harvey <garretth@tuta.io>
2022-01-21 11:27:05 -0500

    Kaise fixed my stuff

--- mocking/v2/mock.go
+++ mocking/v2/mock.go
@@ -7,6 +7,50 @@ import (
 	"time"
 )
 
+const write = "write"
+const sleep = "sleep"
+
+type Sleeper interface {
+	Sleep()
+}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
--- mocking/v2/mock_test.go
+++ mocking/v2/mock_test.go
@@ -7,51 +7,6 @@ import (
 	"time"
 )
 
-const write = "write"
-const sleep = "sleep"
-
-type Sleeper interface {
-	Sleep()
-}
-
-type SpySleeper struct {
-	Calls int
-}
-
-type SpyCountdownOperations struct {
-	Calls []string
-}
-
-type ConfigurableSleeper struct {
-	duration time.Duration
-	sleep    func(time.Duration)
-}
-
-type SpyTime struct {
-	durationSlept time.Duration
-}
-
-func (c *ConfigurableSleeper) Sleep() {
-	c.sleep(c.duration)
-}
-
-func (s *SpyTime) Sleep(duration time.Duration) {
-	s.durationSlept = duration
-}
-
-func (s *SpyCountdownOperations) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 func TestConfigurableSleeper(t *testing.T) {
 	sleepTime := 5 * time.Second
 
